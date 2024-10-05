1cd8948d23d6d0083803617bbe8c687e2c9d0260
Pravin nandankar <pravinnandankar03@gmail.com>
2024-10-05 09:18:42 +0530

    Modified images and screens

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -8,11 +8,11 @@ const profile = () => {
       {/* Profile Header */}
       <View style={styles.profileHeader}>
         <Image
-          source={{ uri: 'https://via.placeholder.com/150' }}
+          source={{ uri: 'https://media.licdn.com/dms/image/v2/D5603AQGwb2f1ONSERg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1725645542157?e=1733356800&v=beta&t=UD9Z__PTaGmL2u5Pbd8xnRecG6hyFIz4MvVWpwFGGoM' }}
           style={styles.profileImage}
         />
-        <Text style={styles.username}>John Doe</Text>
-        <Text style={styles.email}>johndoe@example.com</Text>
+        <Text style={styles.username}>Pravin Nandankar</Text>
+        <Text style={styles.email}>pravin@gmail.com</Text>
       </View>
 
       {/* Profile Options */}
